7f1b5927d87d25275d8c769f6dae48ffd97cc0ec
Michael <mschiumo18@gmail.com>
2023-10-29 17:39:35 -0400

    preline working

--- aspiration-station/tailwind.config.js
+++ aspiration-station/tailwind.config.js
@@ -1,11 +1,15 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+  darkMode: 'class',
   content: [
     "./src/**/*.{js,jsx,ts,tsx}",
+    'node_modules/preline/dist/*.js',
   ],
   theme: {
     extend: {},
   },
-  plugins: [],
+  plugins: [
+    require('preline/plugin'),
+  ],
 }
 
