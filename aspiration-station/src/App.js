c7e4619c5d9fad72267646c2dff95c065205ffea
Michael <mschiumo18@gmail.com>
2023-10-30 22:53:20 -0400

    col grid

--- aspiration-station/src/App.js
+++ aspiration-station/src/App.js
@@ -1,12 +1,14 @@
 import "./App.css";
 import Header from "./components/Header.js";
 import BlockQuote from "./components/BlockQuote";
+import Container from "./components/Container";
 
 function App() {
   return (
     <div id="container">
       <Header />
       <BlockQuote />
+      <Container />
     </div>
   );
 }
--- aspiration-station/src/components/Container.js
+++ aspiration-station/src/components/Container.js
@@ -3,7 +3,13 @@ import "./Container.css";
 
 const Container = () => {
   return (
-
+    <div>
+      <div class="grid grid-cols-12 gap-3 mt-10 px-20">
+        <div class="col-span-4 text-center">1</div>
+        <div class="col-span-4 text-center">1</div>
+        <div class="col-span-4 text-center">1</div>
+      </div>
+    </div>
   );
 };
 
