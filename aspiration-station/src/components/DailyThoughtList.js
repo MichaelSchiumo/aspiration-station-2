a45f7ae13bafee409fa2737386c35c224e1a8376
Michael <mschiumo18@gmail.com>
2023-11-05 16:26:57 -0500

    comment

--- aspiration-station/src/components/DailyThoughtList.js
+++ aspiration-station/src/components/DailyThoughtList.js
@@ -1,6 +1,6 @@
 const DailyThoughtList = ({ thoughts }) => {
   //how should we display daily thoughts? Should it be a separate page?
-  return <div>hi</div>;
+  return <div>need to think of a design that makes sense for this</div>;
 };
 
 export default DailyThoughtList;
