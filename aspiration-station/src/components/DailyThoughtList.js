412e2c3c21dbf214a5c1a2be15ac214ebbfa9480
Michael <mschiumo18@gmail.com>
2023-11-05 16:31:05 -0500

    perhaps have this as a separate page with a nav?

--- aspiration-station/src/components/DailyThoughtList.js
+++ aspiration-station/src/components/DailyThoughtList.js
@@ -1,6 +1,11 @@
 const DailyThoughtList = ({ thoughts }) => {
   //how should we display daily thoughts? Should it be a separate page?
-  return <div>need to think of a design that makes sense for this</div>;
+  return (
+    <div>
+      <p>Your thought of the day</p>
+      <button>Click here to see more thoughts</button>
+    </div>
+  );
 };
 
 export default DailyThoughtList;
